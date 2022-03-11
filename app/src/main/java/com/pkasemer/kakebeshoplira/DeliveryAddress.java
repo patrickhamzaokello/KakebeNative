6bb23950d6c2a3b5d2eddfbef1a614349b3fbb7c
alyanga_hafiz <pkasemer@gmail.com>
2022-03-11 14:20:41 +0300

    addess showing up'

---
+++ app/src/main/java/com/pkasemer/kakebeshoplira/DeliveryAddress.java
@@ -0,0 +1,14 @@
+package com.pkasemer.kakebeshoplira;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeliveryAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_address);
+    }
+}
